de17cd3b0bf861ecdb96bba36119a994f3bfba62
Dave Jornales <plusultrastral@gmail.com>
2021-11-16 16:04:44 +0800

    Update Practice.c

--- Practice.c
+++ Practice.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 
 int main(){
+    int a,b;
+    printf("Enter two numbers");
+    scanf("%d%d", &a, &b);
+    int c = a+b;
 
-    printf("Practice lang ito");
+    printf("%d", c);
     printf("mama mo kalbo");
     printf("jc1 and jc2");
 
