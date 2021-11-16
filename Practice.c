9c2f8638bc8d35e15da5dfe1d0fdbc4587e3629a
arsenicsalangsang <salangsang99@gmail.com>
2021-11-16 15:48:40 +0800

    .

--- Practice.c
+++ Practice.c
@@ -2,14 +2,10 @@
 
 int main(){
 
-<<<<<<< HEAD
     printf("Practice lang ito");
     printf("mama mo kalbo");
     printf("Bule kalbo");
-=======
-    printf("Practice lang ito, mam may labyou");
->>>>>>> 1a66f443b69bc239a614f87e588bd51071f3f89a
+    printf("allen quiambao");
 
 return 0;
 }
-
